87066875f043ec0278d407767863ef5ab86894a3
Reza Kiani <rkm7232@gmail.com>
2022-08-15 12:44:41 +0430

    Initial Commit

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,32 @@
+import { defineUserConfig } from 'vuepress'
+import KnznTheme from 'vuepress-theme-knzn'
+import { particlesOptions } from './particlesOptions'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'This Ptr {Personal Blog}',
+  description: 'Reza Kiani\'s Personal Blog',
+  dest: './docs',
+  // debug: true,
+  theme: KnznTheme({
+    backgroundImage: '/images/bg.jpg',
+    darkBackgroundImage: '/images/bg-dark.jpg',
+    darkPostImage: '/images/post-dark.svg',
+    particlesOptions,
+
+    blogger: 'Reza Kiani',
+    slogan: 'Personal Blog And Programming Notes.',
+    avatar: '/images/avatar.jpg',
+    darkAvatar: '/images/avatar-dark.jpg',
+    medias: [
+      { link: 'mailto:rkm7232@gmail.com', icon: 'email' },
+      { link: 'https://github.com/rkm7232/', icon: 'github' },
+    ],
+	beian: 'With ❤️ From Iran',
+    perPage: 10,
+    maxCategories: 6,
+    maxTags: 10,
+	navbar: [],
+    siteStartDate: '2022'
+  }),
+})
---
+++ src/blog/2022-8-12-a-new-start.md
@@ -0,0 +1,9 @@
+---
+title: Hello World
+author: Reza
+date: 08/12/2022
+---
+
+Finally! After so many years of studying/examining different programming languages, I decided to write a blog of mine, again after so many unsuccessful starts. In this blog, I will write mostly about programming, software, hardware, and Robotics, including a few unrelated subjects as well.
+<!-- more -->
+<!-- more -->
