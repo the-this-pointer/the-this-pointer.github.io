7b1e522ea0961c48bda61aa834b26742fdc019a9
Reza Kiani <rkm7232@gmail.com>
2022-08-22 22:14:47 +0430

    fix: google analytics and some chinese words in template

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,28 +4,50 @@ import { particlesOptions } from './particlesOptions'
 
 export default defineUserConfig({
   lang: 'en-US',
-  title: 'This Ptr <Personal Blog/>',
-  description: 'Reza Kiani\'s Personal Blog',
+  title: 'Rsomething <Personal Blog/>',
+  description: 'Rsomething\'s Personal Blog',
   dest: './docs',
   // debug: true,
+  head: [
+    [
+      'script',
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-2R87B0XZCL',
+      },
+    ],
+    [
+      'script',
+      {},
+      `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){
+          dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-2R87B0XZCL');
+      `,
+    ]
+  ],
   theme: KnznTheme({
     backgroundImage: '/images/bg.jpg',
     darkBackgroundImage: '/images/bg-dark.jpg',
     darkPostImage: '/images/post-dark.svg',
     particlesOptions,
 
-    blogger: 'Reza Kiani',
+    blogger: 'Rsomething',
     slogan: 'Personal Blog And Programming Notes.',
     avatar: '/images/avatar.jpg',
     darkAvatar: '/images/avatar-dark.jpg',
     medias: [
-      { link: 'mailto:rkm7232@gmail.com', icon: 'email' },
-      { link: 'https://github.com/rkm7232/', icon: 'github' },
+      { link: 'mailto:www.rkiani@gmail.com', icon: 'email' },
+      { link: 'https://github.com/the-this-pointer/', icon: 'github' },
     ],
 	beian: 'With ❤️ From Iran',
-    perPage: 10,
-    maxCategories: 6,
-    maxTags: 10,
+  beianUrl: 'https://thisptr.com',
+  perPage: 10,
+  maxCategories: 6,
+  maxTags: 10,
 	navbar: [],
     siteStartDate: '2022'
   }),
